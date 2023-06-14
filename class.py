1c88c7eeca5d746212600e023c81a5febb12be11
AhedMayas <ahedmayas5@gmail.com>
2023-06-14 12:23:24 +0300

    feat:class user

---
+++ class.py
@@ -0,0 +1,20 @@
+class User:
+    __name =''
+    __password =''
+    def __init__(this, name, passo) -> None:
+        this.__name=name
+        this.__password = passo
+
+    def name(this):
+        return this.__name
+    
+
+
+u1= User("Ahed","123")
+print(u1.name())
+
+
+
+
+
+    
